440983adca172115964eee3873860d932bd586d6
mirrortom <mirrortom@outlook.com>
2025-05-28 12:50:15 +0800

    bug修正

--- CavLib/cavlib.js
+++ CavLib/cavlib.js
@@ -4,73 +4,73 @@
 //=================================================================================//
 // 
 ((win) => {
-    "use strict";
-    /**
-     * 获取canvalib库的操作对象,包含当前操作的canvas元素属性"this.canvas",和2d对象"this.ctx",和库提供的工具方法
-     * @param {string} canvasId canvasDom的id
-     * @returns {any} 返回this
-     */
-    function canvalib(canvasId) {
-        this.canvas = document.getElementById(canvasId);
-        if (!this.canvas)
-            throw new Error('canvasId not found');
-        //
-        this.ctx = this.canvas.getContext('2d');
-        //
-        return this;
+  "use strict";
+  /**
+   * 获取canvalib库的操作对象,包含当前操作的canvas元素属性"this.canvas",和2d对象"this.ctx",和库提供的工具方法
+   * @param {string} canvasId canvasDom的id
+   * @returns {any} 返回this
+   */
+  function canvalib(canvasId) {
+    this.canvas = document.getElementById(canvasId);
+    if (!this.canvas)
+      throw new Error('canvasId not found');
+    //
+    this.ctx = this.canvas.getContext('2d');
+    //
+    return this;
+  }
+  /**
+   * 画布元素.给canva元素设置style
+   * @param {number} num 风格代码 0,1,..0(博客用的风格)
+   * @returns {any} 返回this
+   */
+  canvalib.prototype.style = function (num = 0) {
+    if (num == 0) {
+      this.canvas.style.display = 'block';
+      //this.canv.style.margin = 'auto';
+      this.canvas.style.backgroundColor = '#dfdfdf';
     }
-    /**
-     * 画布元素.给canva元素设置style
-     * @param {number} num 风格代码 0,1,..0(博客用的风格)
-     * @returns {any} 返回this
-     */
-    canvalib.prototype.style = function (num = 0) {
-        if (num == 0) {
-            this.canvas.style.display = 'block';
-            //this.canv.style.margin = 'auto';
-            this.canvas.style.backgroundColor = '#f0f0f0';
-        }
-        return this;
+    return this;
+  }
+  //=================================
+  // 定义几个常用属性
+  // defineProperty文档: https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Object/defineProperty
+  //=================================
+  // 获取画布宽度
+  Object.defineProperty(canvalib.prototype, 'width', {
+    get() {
+      return this.ctx.canvas.width;
+    },
+    set(val) {
+      this.ctx.canvas.width = val;
     }
-    //=================================
-    // 定义几个常用属性
-    // defineProperty文档: https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Object/defineProperty
-    //=================================
-    // 获取画布宽度
-    Object.defineProperty(canvalib.prototype, 'width', {
-        get() {
-            return this.ctx.canvas.width;
-        },
-        set(val) {
-            this.ctx.canvas.width = val;
-        }
-    });
-    // 获取画布高度
-    Object.defineProperty(canvalib.prototype, 'height', {
-        get() {
-            return this.ctx.canvas.height;
-        },
-        set(val) {
-            this.ctx.canvas.height = val;
-        }
-    });
+  });
+  // 获取画布高度
+  Object.defineProperty(canvalib.prototype, 'height', {
+    get() {
+      return this.ctx.canvas.height;
+    },
+    set(val) {
+      this.ctx.canvas.height = val;
+    }
+  });
 
-    //================================================================================//
-    // 工厂函数factory,返回canvalib对象
-    // 其它静态方法都绑定在factory上
-    //================================================================================//
-    let factory = (canvasId) => {
-        return new canvalib(canvasId);
-    };
-    /**
-     * 为canvalib对象添加实例方法 prototype
-     * @param {any} json 一个方法名和函数值的json对像.方法名要用""号包起来.
-     */
-    factory.extend = (json) => {
-        for (var name in json) {
-            canvalib.prototype[name] = json[name];
-        }
-    };
+  //================================================================================//
+  // 工厂函数factory,返回canvalib对象
+  // 其它静态方法都绑定在factory上
+  //================================================================================//
+  let factory = (canvasId) => {
+    return new canvalib(canvasId);
+  };
+  /**
+   * 为canvalib对象添加实例方法 prototype
+   * @param {any} json 一个方法名和函数值的json对像.方法名要用""号包起来.
+   */
+  factory.extend = (json) => {
+    for (var name in json) {
+      canvalib.prototype[name] = json[name];
+    }
+  };
 // 一些帮助方法
 /**
  * 检查参数个数,返回2个点的坐标.一个对象,p{ax,ay,bx,by}
